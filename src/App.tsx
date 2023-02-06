7618bac9515ad3eeac59ec679b09868279eaff53
Pedro Foltram <pedroaugustofolb@gmail.com>
2023-02-06 16:51:49 -0300

    construindo estrutura basica

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './Navbar';
+import Router from './Router';
+
+import { setLanguage, verifyPrefeerLanguage } from './services/Langague';
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+}	
+body {
+  background-color: #F4F5F7;
+  height: 100%;
+  overflow-x: hidden;
+  p, span, h1, h2, h3, h4, h5, h6, button, label, div, textarea, input{
+    font-family: 'Work Sans ', sans-serif;
+  }
 }
+html {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  position: relative;
+}
+
+`;
+
+const App = () => {
+
+  if(!verifyPrefeerLanguage()){
+    setLanguage('PT-BR');
+  }
+
+  return (
+    <>
+      <GlobalStyle />
+      <Navbar />
+      <Router />
+    </>
+  );
+};
 
 export default App;
